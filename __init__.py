f9aa5b5b834dbfdfb1f549566b6994d543c377d1
jokepop92 <80516312+jokepop92@users.noreply.github.com>
2024-09-11 10:42:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,8 +35,26 @@ def histogramme():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route('/commits-data/')
+def commits_data():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = urlopen(url)
+    commits_data = json.loads(response.read().decode('utf-8'))
+    
+    # Extraire les dates des commits
+    commit_times = [commit['commit']['author']['date'] for commit in commits_data]
+    
+    # Extraire les minutes
+    minutes = [datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ").strftime('%Y-%m-%d %H:%M') for time in commit_times]
+    
+    # Compter les commits par minute
+    counter = Counter(minutes)
+    results = [{'minute': minute, 'count': count} for minute, count in counter.items()]
+    
+    return jsonify(results=results)
+
 @app.route("/commits/")
-def commits():
+def commits_page():
     return render_template("commits.html")
   
 if __name__ == "__main__":
